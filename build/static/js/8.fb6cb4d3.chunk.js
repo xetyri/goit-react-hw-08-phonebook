1ec64f81c43bc2cf2abf25b9caf5dde598ebbddb
Roman Leshchenko <xetyri2014@gmail.com>
2021-12-04 13:37:33 +0200

    connect netlify

---
+++ build/static/js/8.fb6cb4d3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[8],{206:function(t,e,n){"use strict";n.r(e);n(0);var i=n(3),o={container:{minHeight:"calc(75vh - 100px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center",color:"#FFFFFF"}};e.default=function(){return Object(i.jsx)("div",{style:o.container,children:Object(i.jsx)("h1",{style:o.title,children:"Sorry, nothing was found at this address."})})}}}]);
+//# sourceMappingURL=8.fb6cb4d3.chunk.js.map
